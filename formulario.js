e457f17e515409fbfffba30af56f71be4e0945e8
Carlos Eduardo <c.eduardo.jesus@outlook.com>
2022-07-11 11:17:36 -0500

    codigo de otros 2

---
+++ formulario.js
@@ -0,0 +1,97 @@
+var formulario = document.querySelector("#form")
+
+formulario.onsubmit = function(e) {
+
+  e.prevent();
+  
+  var n = formulario.elements[0]
+  var e = formulario.elements[1]
+  var na = formulario.elements[2]
+
+  var nombre = n.value
+  var edad = e.value
+
+  var i = na.selectedIndex
+  var nacionalidad = na.options[i].value
+  console.log(nombre, edad)
+  console.log(nacionalidad)
+
+  if (nombre.length === 0) {
+    n.classList.add("error")
+  }
+  if (edad < 18 || edad > 120) {
+    e.classList.add("error")
+  }
+
+if (nombre.length > 0 
+  && (edad > 18 
+    && edad < 120) ) {
+  agregarInvitado(nombre, edad, nacionalidad)
+  }
+}
+
+var botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+var corteLinea = document.createElement("br")
+document.body.appendChild(corteLinea)
+document.body.appendChild(botonBorrar);
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina"
+  }
+  else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana"
+  }
+  else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana"
+  }
+  else if (nacionalidad === "per") {
+    nacionalidad = "Peruana"
+  }
+
+var lista = document.getElementById("lista-de-invitados")
+
+var elementoLista = document.createElement("div")
+elementoLista.classList.added("elemento-lista")
+lista.appendChild(elementoLista)
+
+var spanNombre = document.createElement("span")
+var inputNombre = document.createElement("input")
+var espacio = document.createElement("br")
+spanNombre.textContent = "Nombre: "
+inputNombre.value = nombre 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio)
+
+function crearElemento(descripcion, valor) {
+var spanNombre = document.createElement("span")
+var inputNombre = document.createElement("input")
+var espacio = document.createElement("br")
+spanNombre.textContent = descripcion + ": "
+inputNombre.value = valor 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio)
+}
+
+crearElemento("Nombre", nombre)
+crearElemento("Edad", edad)
+crearElemento("Nacionalidad", nacionalidad)
+
+
+var botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+var corteLinea = document.createElement("br")
+elementoLista.appendChild(corteLinea)
+elementoLista.appendChild(botonBorrar);
+
+ botonBorrar.onclick = function() {
+// this.parentNode.style.display = 'none';
+botonBorrar.parentNode.remove()
+  }
+}
